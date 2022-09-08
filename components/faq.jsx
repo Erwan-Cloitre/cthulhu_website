dd6558bc6c6a173c4018201f4884e12b392c17b2
Erwan-Cloitre <erwan.cloitre@gmail.com>
2022-09-09 01:43:20 +0200

    V2+Fix2

--- components/faq.jsx
+++ components/faq.jsx
@@ -4,7 +4,7 @@ const Faq = () => {
     <div className="wrapper">
       <ul id="accordion">
           <li>
-              <label for="first">Ma premiere question ? <span>&#x3e;</span></label>
+              <label htmlFor="first">Ma premiere question ? <span>&#x3e;</span></label>
               <input type="radio" name="accordion" id="first"></input>
               <div className="answer">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -12,7 +12,7 @@ const Faq = () => {
               </div>
           </li>
           <li>
-              <label for="second">Ma premiere question ? <span>&#x3e;</span></label>
+              <label htmlFor="second">Ma premiere question ? <span>&#x3e;</span></label>
               <input type="radio" name="accordion" id="second"></input>
               <div className="answer">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -20,7 +20,7 @@ const Faq = () => {
               </div>
           </li>
           <li>
-              <label for="third">Ma premiere question ? <span>&#x3e;</span></label>
+              <label htmlFor="third">Ma premiere question ? <span>&#x3e;</span></label>
               <input type="radio" name="accordion" id="third"></input>
               <div className="answer">
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
