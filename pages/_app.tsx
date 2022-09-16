d5ac5ac593cec5e9b447d64205a45138b2bf687c
Erwan-Cloitre <erwan.cloitre@gmail.com>
2022-09-16 14:08:22 +0200

    Fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import type { AppProps } from "next/app";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
-import { Helmet } from "react-helmet";
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Mumbai;
@@ -10,10 +9,6 @@ const activeChainId = ChainId.Mumbai;
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
-      <Helmet>
-        <title>Eternal Cthulhu</title>
-        <meta name="Eternal Cthulhu is a NFT collection on ethereum blockchain" content="Eternal Cthulhu NFT" />
-      </Helmet>
        <Navbar />
       <Component {...pageProps} />
     </ThirdwebProvider>
