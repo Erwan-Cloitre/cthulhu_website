bc01fa98692139abc3b7062194af8acfb9003ce0
Erwan-Cloitre <erwan.cloitre@gmail.com>
2022-09-16 14:47:21 +0200

    ResetCommit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import type { AppProps } from "next/app";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
-import Meta from "../components/Meta";
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Mumbai;
@@ -10,7 +9,6 @@ const activeChainId = ChainId.Mumbai;
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
-       <Meta />
        <Navbar />
       <Component {...pageProps} />
     </ThirdwebProvider>
