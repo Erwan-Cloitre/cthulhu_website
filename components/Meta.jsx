d68c5a2249e87a1c904c3ecee211e000c58e97e1
Erwan-Cloitre <erwan.cloitre@gmail.com>
2022-09-16 14:15:30 +0200

    FixMETA

---
+++ components/Meta.jsx
@@ -0,0 +1,11 @@
+import { Helmet } from "react-helmet";
+const Meta = () => {
+   return (
+    <Helmet>
+      <title>Eternal Cthulhu</title>
+      <meta name="Eternal Cthulhu is a NFT collection on ethereum blockchain" content="Eternal Cthulhu NFT" />
+    </Helmet>
+   );
+ };
+ 
+ export default Meta;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
+import Meta from "../components/Meta";
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Mumbai;
@@ -9,6 +10,7 @@ const activeChainId = ChainId.Mumbai;
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
+       <Meta />
        <Navbar />
       <Component {...pageProps} />
     </ThirdwebProvider>
