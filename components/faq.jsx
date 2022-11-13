b58cae547c545549537cc0f5b6f635de9c2934e8
Guigztheoldone <guillaume.delawoevre@hotmail.com>
2022-11-13 19:04:46 +0100

    FAQ 4.0

--- components/faq.jsx
+++ components/faq.jsx
@@ -7,8 +7,8 @@ const Faq = () => {
               <label htmlFor="first">What is Eternal Cthulhu Cultist Club ? <span>&#x3e;</span></label>
               <input type="radio" name="accordion" id="first"></input>
               <div className="answer">
-                <p>We are one group of cultist who worships Cthulhu and Ethereum. 
-Our goal is to share our knowledge of the blockchain and the great old ones. 
+                <p>We are one group of cultist who worships Cthulhu and Ethereum. <br></br>
+Our goal is to share our knowledge of the blockchain and the great old ones. <br></br>
 We are investigator, artist, developer, writer and dreamer all around the world who go straight on throw the unknown.
                 </p>
               </div>
@@ -17,9 +17,9 @@ We are investigator, artist, developer, writer and dreamer all around the world 
               <label htmlFor="second">How to join the cult ? <span>&#x3e;</span></label>
               <input type="radio" name="accordion" id="second"></input>
               <div className="answer">
-                <p>Get one eternal cthulhu NFT
-To do it, get the Metamask Chrome or Firefox extension. Load it with ETH through services that allow you to change your money to ETH like Binance, Coinbase, Kraken... 
-Then, click the button MINT  to connect your wallet and approve the transaction on Metamask. 
+                <p>Get one eternal cthulhu NFT<br></br>
+To do it, get the Metamask Chrome or Firefox extension. Load it with ETH through services that allow you to change your money to ETH like Binance, Coinbase, Kraken... <br></br>
+Then, click the button MINT  to connect your wallet and approve the transaction on Metamask. <br></br>
 To conclude, believe in ETH and the great old ones
                 </p>
               </div>
@@ -28,12 +28,12 @@ To conclude, believe in ETH and the great old ones
               <label htmlFor="third">What happens when I enter the cult ?<span>&#x3e;</span></label>
               <input type="radio" name="accordion" id="third"></input>
               <div className="answer">
-                <p>- Access to cultist community 
-receiving token from our lord and savior cthulhu
-Receiving air drop and relics
-invitation for secret event
-more secret incoming
-Have faith in ethereum and Cthulhu
+                <p>- Access to cultist community <br></br>
+- receiving token from our lord and savior cthulhu<br></br>
+- Receiving air drop and relics<br></br>
+- Invitation for secret event<br></br>
+- More secret incoming<br></br>
+- Have faith in ethereum and Cthulhu<br></br>
                 </p>
               </div>
           </li>
