0a28b5428168c477ae79c067e6d4c93d137713da
Erwan-Cloitre <erwan.cloitre@gmail.com>
2022-11-20 01:39:27 +0100

    Test meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,6 @@ function MyApp({ Component, pageProps }: AppProps) {
       <html lang="en" />
       <title lang="en">The Eternal Cthulhu NFT collection</title>
       <link rel="canonical" href="http://www.eternalcthulhu.com" />
-      <link title="Cthulhu Mint" rel="alternate" href="https://www.eternalcthulhu.com/mint" />
       <meta name="description" content="Eternal Cthulhu is a NFT collection based on ethereum blockchain" />
       <meta name="keywords" content="NFT, ETH, ETHEREUM, CTHULHU, ETERNAL, CRYPTO, MINT, STAKING, COLLECTION, OPENSEA"></meta>
       <meta name="author" content="@Pinpin.eth"></meta>
