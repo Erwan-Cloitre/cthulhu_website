898b09399fc362d689de268fe35bed2f587821d1
Guigztheoldone <guillaume.delawoevre@hotmail.com>
2022-12-10 17:17:14 +0100

    Update faq.jsx
    faq correction

--- components/faq.jsx
+++ components/faq.jsx
@@ -18,7 +18,7 @@ We are investigator, artist, developer, writer and dreamer all around the world 
               <input type="radio" name="accordion" id="second"></input>
               <div className="answer">
                 <p>Get one eternal cthulhu NFT<br></br>
-To do it, get the Metamask Chrome or Firefox extension. Load it with ETH through services that allow you to change your money to ETH like Binance, Coinbase, Kraken... <br></br>
+To do it, get the Metamask Chrome or Firefox extension. Load it with ETH through services that allow you to change your money to ETH like Binance, Coinbase... (don't use FTX) <br></br>
 Then, click the button MINT  to connect your wallet and approve the transaction on Metamask. <br></br>
 To conclude, believe in ETH and the great old ones
                 </p>
@@ -29,11 +29,11 @@ To conclude, believe in ETH and the great old ones
               <input type="radio" name="accordion" id="third"></input>
               <div className="answer">
                 <p>- Access to cultist community <br></br>
-- receiving token from our lord and savior cthulhu<br></br>
+- Collecting token from our lord and savior cthulhu<br></br>
 - Receiving air drop and relics<br></br>
 - Invitation for secret event<br></br>
 - More secret incoming<br></br>
-- Have faith in ethereum and Cthulhu<br></br>
+- Have faith in Ethereum and Cthulhu<br></br>
                 </p>
               </div>
           </li>
